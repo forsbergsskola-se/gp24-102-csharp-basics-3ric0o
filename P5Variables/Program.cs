a08d0e5a4cfa0020e6e3a4023a3ffe6a2b52cdd5
3ric0o <3ric0o99@gmail.com>
2024-09-16 12:02:08 +0200

    add
    P5Variables

---
+++ P5Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string a = "Hello";
+string b = "World";
+string c = "Forsbergs";
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(b);

01d9fd902d94890ec6febec9c278106850588b3e
3ric0o <3ric0o99@gmail.com>
2024-09-16 11:32:15 +0200

    add
    3- Printing Output

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-Console.Write
-    (
-"To Write a sentence here" +
-    ""
-)
-    ;
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+Console.WriteLine("It's easy to print output...");
+Console.WriteLine("...to the console!");

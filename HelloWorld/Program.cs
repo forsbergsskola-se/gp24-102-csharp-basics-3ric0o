b247a62f2e822efc96b0fe12d29efbc01811480b
3ric0o <3ric0o99@gmail.com>
2024-09-16 11:36:52 +0200

    add
    P3PrintingOutput

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,2 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Welcome to 102 CSharp Basics!");
-Console.WriteLine("It's easy to print output...");
-Console.WriteLine("...to the console!");
+

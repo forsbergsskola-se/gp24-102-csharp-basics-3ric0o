2d90828444520494637bbb962e3507173a04efca
3ric0o <3ric0o99@gmail.com>
2024-09-17 16:11:43 +0200

    add
    Nim Game complete

---
+++ Nim/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Welcome to Nim!");
+Console.WriteLine("|||||||||||||||||||||||| (24)");
+
+int userInput = 10;
+int matchesNum = 24;
+bool isPlayer = false;
+Random seed = new Random();
+
+while (matchesNum > 0)
+{
+    int Ai = seed.Next(1, 4);
+    
+    isPlayer = !isPlayer;
+
+    if (isPlayer)
+    {
+        Console.WriteLine("\nHow many matches do you want to draw?");
+        userInput = Int32.Parse(Console.ReadLine());
+
+        while (userInput > 3)
+        {
+            Console.WriteLine("Please enter a number between 1 and 3");
+            userInput = Int32.Parse(Console.ReadLine());
+        }
+    }
+    else
+    {
+            Console.WriteLine("\nThe Ai Draws " + Ai + " matches.");
+    }
+    
+    matchesNum -= isPlayer ? userInput : Ai;
+    if (matchesNum >= 2)
+    {
+        for (int i = 0; i < matchesNum; i++)
+        {
+            Console.Write("|");
+        }
+        Console.Write(" " + matchesNum);
+    }
+    else if (matchesNum < 2)
+    {
+        Console.WriteLine(isPlayer ? "You Win!" : "You Lost!");
+        break;
+    }
+}
--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -3,13 +3,13 @@ Console.WriteLine("What's your age?");
 int age = Int32.Parse( Console.ReadLine());
 
 bool isChild = age <= 12;
-if (isChild) Console.WriteLine("You're a child");
+    if (isChild) Console.WriteLine("You're a child");
 
 bool isTeenager = age >= 13 && age <= 19;
-if (isTeenager) Console.WriteLine("You're a teenager");
+    if (isTeenager) Console.WriteLine("You're a teenager");
 
 bool isAdult = age > 19;
-if (isAdult) Console.WriteLine("You're an adult");
+    if (isAdult) Console.WriteLine("You're an adult");
 
 Console.WriteLine("Give me another integer.");
     int UserInput = Int32.Parse(Console.ReadLine());
